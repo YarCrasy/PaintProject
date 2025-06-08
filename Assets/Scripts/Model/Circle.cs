5c815e462ab4010b14b76f186e5ede48e3a93dd9
YarCrasy <108998893+YarCrasy@users.noreply.github.com>
2025-06-08 22:52:47 +0100

    backup

---
+++ Assets/Scripts/Model/Circle.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Circle : Shape
+{
+    public Circle()
+    {
+        type = ShapeType.Circle;
+    }
+    
+}
--- Assets/Scripts/Model/Shape.cs
+++ Assets/Scripts/Model/Shape.cs
@@ -5,5 +5,7 @@ public enum ShapeType { Point, Line, Circle, RegularShape, IrregularShape, }
 public abstract class Shape
 {
     public ShapeType type;
+    public Vector3 position; // Position in world space and center of the shape
+
 
 }
